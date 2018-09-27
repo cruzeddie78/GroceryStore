651bb04cbd86483d417e5b22bc168d2fad980ae5
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-09-26 17:32:19 -0700

    Created a do while loop and created the condition that if Q and q were entered, the program would end and the if any other letter was entered, the menu would appear again

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+// Eddie Alexis Cruz
 int main()
 {
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
-  cout<<"\n==GROCERY LIST MANAGER==";
+do
+{  cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+} 
+while ( input!='q' && input!='Q' );
   return 0;
 }
