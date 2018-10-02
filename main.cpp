4136e45cc0c27e74aad61ae79af6c1e9ced4758e
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-10-01 17:56:57 -0700

    displayed the list that we had made using an IF statement and conditions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
 do
 {  cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
@@ -14,7 +15,37 @@ do
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-} 
-while ( input!='q' && input!='Q' );
-  return 0;
+   if (input=='A' || input== 'a')
+{ cout<<"What is the item?"<<endl;
+  cin>>item;
+
+if (numItems < 5)
+{ list[numItems]=item;
+        numItems++;
+
+}
+else 
+{ cout<<"You'll need a bigger list!"<<endl;
+
+}
+
+
+}}while ( input!='q' && input!='Q' );
+
+cout<<"\n==ITEMS TO BUY=="<<endl;
+
+for (int index=0; index <5; index++)
+{
+        string converted= list[index]; 
+        cout<<index+1;
+        cout<<" ";
+        cout<<converted<<endl;
+                
+}
+
+
+
+
+return 0;
+  
 }
