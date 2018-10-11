595f100457279d2d832b9671965898557f2db68f
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-10-10 17:31:35 -0700

    We got the program to delete the last item using pop back function from the vector library

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@ do
 {  cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
+  if (list.size()>0)
+  {cout<< "\n (D)elete last item";
+  }
+
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
@@ -22,8 +26,16 @@ do
 { cout<<"What is the item?"<<endl;
   cin>>item;
 
-list.push_back(item);
+ list.push_back(item);
+
 }
+  if(input=='D' || input=='d')
+  {
+    cout<< list[list.size()-1]<<" was deleted from the list. \n";
+
+    list.pop_back();
+    
+  }
 
 }while ( input!='q' && input!='Q' );
   
